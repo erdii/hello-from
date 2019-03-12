908a2aa1be8922cf3fddecd84b5fee6ab9c418aa
erdii <erdiicodes@gmail.com>
2019-03-12 09:49:44 +0100

    json response

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const os = require("os");
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 
+app.use(morgan("combined"));
+
 app.get("/", (req, res) => {
-  res.send("Hello " + req.ip + " from " + os.hostname());
+  res.send({
+    message: `Hello from ${os.hostname()}`,
+    hostname: os.hostname(),
+    ip: req.ip,
+    ts: Date.now(),
+  });
 });
 
 app.listen(3000, () => {
