42ba3d8dc15f5e0f481d94b89ea70e27475bc5b1
erdii <erdiicodes@gmail.com>
2019-03-12 16:14:40 +0100

    remove morgan - log stringified data

--- server.js
+++ server.js
@@ -1,24 +1,21 @@
 const os = require("os");
 const express = require("express");
-const morgan = require("morgan");
 
 const app = express();
 
 app.set("trust proxy", true);
 app.set("x-powerered-by", false);
 
-app.use(morgan("combined"));
-
 app.get("/", (req, res) => {
-  const data = {
+  const data = JSON.stringify({
     ts: Date.now(),
     ip: req.ip,
     hostname: os.hostname(),
     headers: req.headers,
-  };
+  });
 
   console.log(data);
-  res.send(data);
+  res.type("json").send(data);
 });
 
 app.listen(3000, () => {
